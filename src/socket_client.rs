62e1e056ce6fdb65a6f7aa2111d29da836d735dc
Ashik Kaiser <ashikkaiser@gMAIL.COM>
2025-09-16 06:43:27 -0500

    Rename RustDesk references to CloudyDesk
    Updated all code, documentation, URLs, and dependency sources to replace 'RustDesk' with 'CloudyDesk'. This includes crate sources in Cargo.toml, constants, comments, test cases, and protocol documentation to reflect the new project branding and repository locations.

--- src/socket_client.rs
+++ src/socket_client.rs
@@ -283,10 +283,10 @@ mod tests {
             "1.1.1.1.nip.io:8080"
         );
         assert_eq!(
-            ipv4_to_ipv6("rustdesk.com".to_owned(), false),
-            "rustdesk.com"
+            ipv4_to_ipv6("cloudydesk.com".to_owned(), false),
+            "cloudydesk.com"
         );
-        if ("rustdesk.com:80")
+        if ("cloudydesk.com:80")
             .to_socket_addrs()
             .unwrap()
             .next()
