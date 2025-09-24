416010c1f239c12d90d1f190bb2b204006f62fc3
Ashik Kaiser <ashikkaiser@gMAIL.COM>
2025-09-23 23:18:28 -0500

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.cloudydesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["cd-hb.cloudydesk.us"];
+pub const RS_PUB_KEY: &str = "RIUIXVGow6gLsgXst710AOIf7KV+PcQhbC1l227GLSI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
